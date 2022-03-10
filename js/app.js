817866bb8c9da6d80d7b5cf3d70a86cef942bac4
SabbirSami <smd71430@gmail.com>
2022-03-10 22:48:37 +0600

    like button problem solve

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.pust(id);
+    likedPostsId.push(id);
     showPosts(posts);
 };
 
@@ -55,6 +55,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+    console.log(post);
     const image = post.image;
     const div = document.createElement("article");
     div.classList.add("post");
