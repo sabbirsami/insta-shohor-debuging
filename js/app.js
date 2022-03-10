eb80ce99c40316a4cada04207b64e48fb41928f9
SabbirSami <smd71430@gmail.com>
2022-03-10 22:38:01 +0600

    profile picture fix

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id);
+    likedPostsId.push(id);
     showPosts(posts);
 };
 
@@ -66,7 +66,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
