dfa0a43e887e4d85a994b80c6aef40cb27cb5137
SabbirSami <smd71430@gmail.com>
2022-03-10 22:40:47 +0600

    discription text error fix

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.push(id);
+    likedPostsId.pust(id);
     showPosts(posts);
 };
 
@@ -30,7 +30,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
     return text.length < 30
-        ? "text"
+        ? text
         : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
